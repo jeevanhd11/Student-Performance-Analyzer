5a3284a6eae77d34facc716aa2e5637f5a393a8d
Jeevan H D <hdeevan23@gmail.com>
2026-02-21 13:46:21 +0530

    Phase 3completed: Add visualization and heatmap analysis

--- Readme.md
+++ Readme.md
@@ -16,5 +16,11 @@
 - Dataset contains both academic and personal attributes.
 - Final grade (G3) will be used for prediction.
 
+## Phase 3: Data Visualization
+
+- Visualized grade distribution
+- Analyzed study time impact on final grades
+- Generated correlation heatmap to identify key predictors
+
 ### Project Goal
 To analyze student performance and build a machine learning model to predict final grades based on various factors.
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import pandas as pd 
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 df=pd.read_csv("data/student-mat.csv")
 
@@ -6,3 +8,23 @@ print(df.head())
 
 print("\nDataset Info:\n")
 print(df.info())
+
+plt.figure(figsize=(8,5))
+sns.histplot(df['G3'], bins=20, kde=True)
+plt.title("Distribution of Final Grades (G3)")
+plt.xlabel("Final Grade")
+plt.ylabel("Count")
+plt.show()
+
+plt.figure(figsize=(8,5))
+sns.boxplot(x=df['studytime'], y=df['G3'])
+plt.title("Study Time vs Final Grade")
+plt.xlabel("Study Time")
+plt.ylabel("Final Grade")
+plt.show()
+
+plt.figure(figsize=(12,8))
+numeric_df = df.select_dtypes(include=['int64'])
+sns.heatmap(numeric_df.corr(), annot=False, cmap="coolwarm")
+plt.title("Correlation Heatmap")
+plt.show()
